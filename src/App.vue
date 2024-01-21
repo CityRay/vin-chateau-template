b4eb3c373515d86995d51dcb4522e7c06bc9837a
Ray Lin <ray102467@gmail.com>
2024-01-21 22:09:00 +0800

    Add Files

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <HeadComponent />
+  <RouterView />
+  <FooterComponent />
+</template>
+
+<script>
+import HeadComponent from '@/components/HeadComponent.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
+
+export default {
+  components: {
+    HeadComponent,
+    FooterComponent
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/all.scss";
+</style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="mt-3">
+    <p>
+      Vite template built by
+      <a href="#">@Template</a>.
+    </p>
+    <p class="mb-0">
+      <a href="#">Back to top</a>
+    </p>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  name: 'FooterComponent',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  padding: 2.5rem 0;
+  color: #727272;
+  text-align: center;
+  background-color: #f9f9f9;
+  border-top: 0.05rem solid #e5e5e5;
+}
+</style>
